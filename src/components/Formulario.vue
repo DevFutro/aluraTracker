ca59015319dc7def00284cfd2a7f8523b07fb9fc
DevFutro <141151628+DevFutro@users.noreply.github.com>
2023-08-01 11:30:36 -0300

    Initial commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="box formulario">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from './Temporizador.vue'
+
+export default defineComponent({
+  name: "FormulárioPreencher",
+  emits: [ 'aoSalvarTarefa' ],
+  components: {
+    Temporizador
+  },
+  data () {
+    return {
+      descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa (tempoDecorrido: number) : void {
+      console.log('tempo da tarefa', tempoDecorrido)
+      console.log('descrição da tarefa', this.descricao)
+      this.$emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoDecorrido,
+        descricaoTarefa: this.descricao
+      })
+      this.descricao = ''
+    }
+  }
+});
+</script>
+
+<style>
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
+</style>
---
+++ src/interfaces/ITarefa.ts
@@ -0,0 +1,4 @@
+export default interface ITarefa {
+    duracaoEmSegundos: number,
+    descricaoTarefa: string
+}
